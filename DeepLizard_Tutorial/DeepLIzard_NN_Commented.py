a6d3013ee6c2d0142041b9c79cf88bf78da7d02f
george-hummus <72563032+george-hummus@users.noreply.github.com>
2020-10-27 17:49:36 +0000

    Adjusting NN so loss minimises
    Removed softmax function from output layer meant that the loss over epochs now does decrease as it should.
    Also made variation on DeepLizard NN with less comments so it is quicker to navigate (still kept full commented one - which has had its name changed to reflect this)

--- DeepLizard_Tutorial/DeepLIzard_NN_Commented.py
+++ DeepLizard_Tutorial/DeepLIzard_NN_Commented.py
@@ -8,19 +8,37 @@ DeepLizard Tutorial in making a neural network
 ### IMPORTS ### 
 
 import torch
-torch.set_grad_enabled(True) 
+#The top-level PyTorch package and tensor library. 
+torch.set_grad_enabled(True) #turns on/off computational graph needed for training
 
 import torch.nn as nn
-
+#A subpackage that contains modules and extensible classes for building
+#neural networks. 
 import torch.optim as optim
-
+#A subpackage that contains standard optimization operations like SGD and Adam. 
 import torch.nn.functional as F
+#A functional interface that contains typical operations used for building
+#neural networks like loss functions and convolutions. 
 
 import torchvision
-
+#A package that provides access to popular datasets, model architectures, 
+#and image transformations for computer vision. 
 import torchvision.transforms as transforms 
+#An interface that contains common transforms for image processing. 
 
+#tensorboard utility package that enables us to use TensorBoard with PyTorch.
+#enables us to create classes that can send data to tensorboard to display 
 from torch.utils.tensorboard import SummaryWriter
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from sklearn.metrics import confusion_matrix
+#from plotcm import plot_confusion_matrix
+#above is a local file that'll be used later
+
+import pdb #python debugger 
 
 torch.set_printoptions(linewidth=120) 
 #sets the print options for PyTorch print statements. 
@@ -68,32 +86,51 @@ class Network(nn.Module):
         # (2) hidden conv layer (all layers except in & out are 'hidden')
         t = self.conv1(t) #tensor operated on by conv1 layer
         t = F.relu(t) #rectifer activation function
+        #turns all -ve values to zero and keeps all =ve values the same
         t = F.max_pool2d(t, kernel_size=2, stride=2) #max pooling operation
-        
+        #reduces the number of pixels in the image by disregarding the 
+        #lower pixel count in the filter and retaining the max
+        #in this case the filter size is 2x2 and the stride size (no. of rows
+        #ypu go down each time) is also zero, so it will half the image size
         
         # (3) hidden conv layer
         t = self.conv2(t) #tensor operated on by conv2 layer
-        t = F.relu(t) 
-        t = F.max_pool2d(t, kernel_size=2, stride=2) 
+        t = F.relu(t) #rectifer activation function
+        t = F.max_pool2d(t, kernel_size=2, stride=2) #max pooling operation
         
         # (4) hidden linear layer
         t = t.reshape(-1, 12 * 4 * 4) #flattens tesnor so that it is 1D
+        #length of tensor is 12*4*4 as output of con2 layer has 12 channels
+        #and each channel has a size of 4x4
         t = self.fc1(t) #pass through fc1 linar layer
         t = F.relu(t) ##rectifer activation function
         
         # (5) hidden linear layer
-        t = self.fc2(t)
-        t = F.relu(t)
+        #don't need to flatten again as output of fc1 will be flat
+        t = self.fc2(t) #pass through fc2 linar layer
+        t = F.relu(t) #rectifer activation function
         
         # (6) output layer
         t = self.out(t) #passes thrugh output layer
+        #t = F.softmax(t, dim=1) #softmax returns a positive probability for 
+        #but dont need this as we just need t to calculate loss
+        
+        #each of the prediction classes, and the probabilities sum to 1. 
+        #where the classes are from 0->9 and correspond to a different item of clothing.
         
         return t
 
 network = Network() #creates an instance of the network
 
+print(network) #will print the string representation of the network
+#can use netowork.layer to get the string representation of a layer
+#can used network.layer.weights so access the weights of a layer
+
 #creating an optimiser to update the weights
 optimiser = optim.Adam(network.parameters(), lr=0.01)
+#passing in network parameters means optimiser can access gradients.
+#lr is the learning rate tells the optimizer how far to step in 
+#the direction of the loss function's minimum
 
 def get_num_correct(p, lbs): #finds the number ofpredictions that are correct
     return p.argmax(dim=1).eq(lbs).sum().item()
@@ -105,8 +142,8 @@ grid = torchvision.utils.make_grid(images) #turns image batch into gird of image
 
 tb = SummaryWriter() 
 #creates summary writer instance call tb, so we can pass data to tesnorboard
-tb.add_image('images', grid) 
-tb.add_graph(network, images) 
+tb.add_image('images', grid) #adds images to tensorboad
+tb.add_graph(network, images) #adds network graph to tesnor board
 
 ### Calculating the Loss ###
 for epoch in range(10): #loops thru for 10 epochs
@@ -124,7 +161,9 @@ for epoch in range(10): #loops thru for 10 epochs
         
         optimiser.zero_grad()#zeros gradients before adding newly calculated ones
     
+        #calculate the loss gradients
         loss.backward() #calculates gradients
+        #gradients are used to update the weights
     
         optimiser.step() # Updating the weights using the gradients
         
